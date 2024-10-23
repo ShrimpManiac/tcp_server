d100132d72f5e01c357199f52ce90d02ff0c9a58
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-10-23 18:06:12 +0900

    update: "initialHandler에서 response 패킷을 보내도록 변경"

--- src/handlers/user/initial.handler.js
+++ src/handlers/user/initial.handler.js
@@ -1,12 +1,18 @@
+import { HANDLER_IDS, RESPONSE_SUCCESS_CODE } from '../../constants/handlerIds.js';
 import { addUser } from '../../session/user.session.js';
+import { createResponse } from '../../utils/response/createResponse.js';
 
 const initialHandler = async ({ socket, userId, payload }) => {
   const { deviceId } = payload;
 
   addUser(socket, deviceId);
 
+  const initialResponse = createResponse(HANDLER_IDS.INITIAL, deviceId, RESPONSE_SUCCESS_CODE, {
+    userId: deviceId,
+  });
+
   // 뭔가 처리가 끝났을 때 보내는 것
-  socket.write('');
+  socket.write(initialResponse);
 };
 
 export default initialHandler;
