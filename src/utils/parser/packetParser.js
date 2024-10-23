5f99480b9004f19ce64eb5310557ccceb197662c
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-10-23 17:30:39 +0900

    update: "패킷 파싱 구현"

--- src/utils/parser/packetParser.js
+++ src/utils/parser/packetParser.js
@@ -1,3 +1,4 @@
+import { getProtoTypeNameByHanderId } from '../../handlers/index.js';
 import { getProtoMessages } from '../../init/loadProtos.js';
 
 export const packetParser = (data) => {
@@ -15,10 +16,24 @@ export const packetParser = (data) => {
   const handlerId = packet.handlerId;
   const userId = packet.userId;
   const clientVersion = packet.clientVersion;
-  const payload = packet.payload;
   const sequence = packet.sequence;
 
   console.log(`clientVersion: ${clientVersion}`);
+
+  const protoTypeName = getProtoTypeNameByHanderId(handlerId);
+  if (!protoTypeName) {
+    console.error(`알 수 없는 핸들러ID: ${handlerId}`);
+  }
+
+  const [namespace, typeName] = protoTypeName.split('.');
+  const PayloadType = protoMessages[namespace][typeName];
+  let payload;
+
+  try {
+    payload = PayloadType.decode(packet.payload);
+  } catch (error) {
+    console.error(error);
+  }
 
   return { handlerId, userId, payload, sequence };
 };
