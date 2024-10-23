d2ecc4bec94a9d1776d1aad27511fcb9bf1f84bc
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-10-23 13:01:18 +0900

    comment: "PACKET_TYPE enum 항목 수정시 주의사항 추가"
    - 의외 패킷관련 함수들의 수정 주의사항을 jsDoc에서 일반 주석으로 변경

--- src/constants/header.js
+++ src/constants/header.js
@@ -1,16 +1,12 @@
 /**
  * 헤더의 길이 부분 크기 (바이트)
- *
- * 수정시 readUInt와 writeUInt 메소드도 수정필요
  */
-export const TOTAL_LENGTH_BYTES = 4;
+export const TOTAL_LENGTH_BYTES = 4; // 수정시 readUInt와 writeUInt 메소드도 수정필요
 
 /**
  * 헤더의 패킷타입 부분 크기 (바이트)
- *
- * 수정시 readUInt와 writeUInt 메소드도 수정필요
  */
-export const PACKET_TYPE_BYTES = 1;
+export const PACKET_TYPE_BYTES = 1; // 수정시 readUInt와 writeUInt 메소드도 수정필요
 
 /**
  * 패킷 타입
@@ -18,4 +14,5 @@ export const PACKET_TYPE_BYTES = 1;
 export const PACKET_TYPE = Object.freeze({
   PING: 0,
   NORMAL: 1,
+  // 추가시 해당 enum을 사용하는 switch문에 case 추가 필요
 });
