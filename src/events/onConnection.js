195dbad9b800ff827f567ee4148781d99119772e
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-10-23 20:44:35 +0900

    log: "로그 메세지 일부 한글로 변경"

--- src/events/onConnection.js
+++ src/events/onConnection.js
@@ -3,7 +3,7 @@ import { onEnd } from './onEnd.js';
 import { onError } from './onError.js';
 
 export const onConnection = (socket) => {
-  console.log(`Client connected from: ${socket.remoteAddress} : ${socket.remotePort}`);
+  console.log(`클라이언트가 연결되었습니다: ${socket.remoteAddress} : ${socket.remotePort}`);
 
   // 각 클라이언트마다 고유한 버퍼를 유지
   socket.buffer = Buffer.alloc(0);
--- src/events/onEnd.js
+++ src/events/onEnd.js
@@ -1,7 +1,7 @@
 import { removeUser } from '../session/user.session.js';
 
 export const onEnd = (socket) => () => {
-  console.log(`Client disconnected: ${socket.remoteAddress} : ${socket.remotePort}`);
+  console.log(`클라이언트 연결이 종료되었습니다: ${socket.remoteAddress} : ${socket.remotePort}`);
 
   // 세션에서 유저 삭제
   removeUser(socket);
