7bc352f38e0f1a50d1a0db30754c4f47cd111fb7
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-10-23 11:31:11 +0900

    bugfix: "서버파일 내 잘못된 함수/변수명 사용 오류 수정"

--- src/events/onData.js
+++ src/events/onData.js
@@ -7,12 +7,12 @@ export const onData = (socket) => (data) => {
 
   while (socket.buffer.length >= totalHeaderLength) {
     const length = socket.buffer.readUInt32BE(0);
-    const packetType = socket.buffer.readUInt8BE(config.packet.totalLength);
+    const packetType = socket.buffer.readUInt8(config.packet.totalLength);
 
     if (socket.buffer.length < length) break; // 아직 전체 패킷이 도착하지 않음
 
     const packet = socket.buffer.subarray(totalHeaderLength);
-    socket.buffer = socket.bugger.subarray(0, totalHeaderLength);
+    socket.buffer = socket.buffer.subarray(0, totalHeaderLength);
     console.log(`length: ${length}, packetType: ${packetType}`);
     console.log(`packet: ${packet}`);
   }
