c7fa8a45c6b7c7f97bf0a5ceff7a3c46b6a98460
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-10-23 11:11:55 +0900

    fix: "ASSET_TYPE value값을 string->number로 단순화"

--- src/constants/assets.js
+++ src/constants/assets.js
@@ -2,7 +2,7 @@
  * enum화 된 게임에셋 타입
  */
 export const ASSET_TYPE = Object.freeze({
-  STAGE: 'stage',
-  ITEM: 'item',
-  ITEM_UNLOCK: 'item_unlock',
+  STAGE: 0,
+  ITEM: 1,
+  ITEM_UNLOCK: 2,
 });
