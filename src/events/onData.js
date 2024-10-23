a1788393db5413f0537372beb027f7a9f17b9ac9
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-10-23 11:06:22 +0900

    update: "onData 데이터 처리 로직 구현"

--- src/events/onData.js
+++ src/events/onData.js
@@ -1,3 +1,19 @@
+import { config } from '../config/config.js';
+
 export const onData = (socket) => (data) => {
-  console.log(data);
+  socket.buffer = Buffer.concat([socket.buffer, data]);
+
+  const totalHeaderLength = config.packet.totalLength + config.packet.typeLength;
+
+  while (socket.buffer.length >= totalHeaderLength) {
+    const length = socket.buffer.readUInt32BE(0);
+    const packetType = socket.buffer.readUInt8BE(config.packet.totalLength);
+
+    if (socket.buffer.length < length) break; // 아직 전체 패킷이 도착하지 않음
+
+    const packet = socket.buffer.subarray(totalHeaderLength);
+    socket.buffer = socket.bugger.subarray(0, totalHeaderLength);
+    console.log(`length: ${length}, packetType: ${packetType}`);
+    console.log(`packet: ${packet}`);
+  }
 };
