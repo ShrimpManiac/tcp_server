f6c7ede617412e5023116fdbbf231bb95608b2b3
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-10-23 20:40:50 +0900

    bugfix: "클라이언트 접속 종료시 세션에서 유저를 삭제하도록 수정"

--- src/events/onEnd.js
+++ src/events/onEnd.js
@@ -1,3 +1,8 @@
+import { removeUser } from '../session/user.session.js';
+
 export const onEnd = (socket) => () => {
   console.log(`Client disconnected: ${socket.remoteAddress} : ${socket.remotePort}`);
+
+  // 세션에서 유저 삭제
+  removeUser(socket);
 };
--- src/events/onError.js
+++ src/events/onError.js
@@ -5,4 +5,7 @@ import { handleError } from '../utils/error/errorHandler.js';
 export const onError = (socket) => (error) => {
   console.error('소켓 오류:', error);
   handleError(socket, new CustomError(500, `소켓 오류: ${error.message}`));
+
+  // 세션에서 유저 삭제
+  removeUser(socket);
 };
