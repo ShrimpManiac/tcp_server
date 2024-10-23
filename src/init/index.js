71f4adb427c072adb84f2c7806f14f6bb5b7e1f9
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-10-23 11:48:49 +0900

    bugfix: "assets.js의 로드할 게임에셋 경로가 잘못되었던 점을 수정"

--- src/init/index.js
+++ src/init/index.js
@@ -2,7 +2,7 @@ import { loadGameAssets } from './assets.js';
 
 const initServer = async () => {
   try {
-    await loadGameAssets;
+    await loadGameAssets();
   } catch (e) {
     console.error(e);
     process.exit(1);
