3cccf871c980f7ef5246f10d80845e581d581581
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-10-23 11:09:37 +0900

    update: "PACKET_TYPE enum 추가"

--- src/constants/header.js
+++ src/constants/header.js
@@ -1,2 +1,7 @@
 export const TOTAL_LENGTH_BYTES = 4; // 헤더의 길이 부분 크기
 export const PACKET_TYPE_BYTES = 4; // 헤더의 패킷타입 부분 크기
+
+export const PACKET_TYPE = Object.freeze({
+  PING: 0,
+  NORMAL: 1,
+});
