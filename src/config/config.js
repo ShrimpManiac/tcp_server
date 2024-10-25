3cb70f3ac953b7c738b5cf91a0cedf2ede8aa23a
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-10-25 17:00:38 +0900

    add: "DB 연동 구현 & 테스트 완료"

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,18 @@
-import { CLIENT_VERSION, HOST, PORT } from '../constants/env.js';
+import {
+  CLIENT_VERSION,
+  DB1_HOST,
+  DB1_NAME,
+  DB1_PASSWORD,
+  DB1_PORT,
+  DB1_USER,
+  DB2_HOST,
+  DB2_NAME,
+  DB2_PASSWORD,
+  DB2_PORT,
+  DB2_USER,
+  HOST,
+  PORT,
+} from '../constants/env.js';
 import { PACKET_TYPE_BYTES, TOTAL_LENGTH_BYTES } from '../constants/header.js';
 
 export const config = {
@@ -12,5 +26,22 @@ export const config = {
   packet: {
     totalLength: TOTAL_LENGTH_BYTES,
     typeLength: PACKET_TYPE_BYTES,
+  },
+  database: {
+    GAME_DB: {
+      name: DB1_NAME,
+      user: DB1_USER,
+      password: DB1_PASSWORD,
+      host: DB1_HOST,
+      port: DB1_PORT,
+    },
+    USER_DB: {
+      name: DB2_NAME,
+      user: DB2_USER,
+      password: DB2_PASSWORD,
+      host: DB2_HOST,
+      port: DB2_PORT,
+    },
+    // 필요한 만큼 추가
   },
 };
