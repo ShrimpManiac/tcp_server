ff1648a51abe5a066f8892a1822a86f04e4a7dce
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-10-26 20:45:32 +0900

    add: "Game 클래스"

---
+++ src/classes/models/game.class.js
@@ -0,0 +1,40 @@
+import { GAME_STATE, MAX_PLAYERS } from '../../constants/game.js';
+
+class Game {
+  constructor(id) {
+    this.id = id;
+    this.users = [];
+    this.state = GAME_STATE.WAITING;
+  }
+
+  addUser(user) {
+    if (this.users.length > MAX_PLAYERS) {
+      throw new Error(`Game session is full.`);
+    }
+    this.users.push(user);
+
+    if (this.users.length === MAX_PLAYERS) {
+      setTimeout(() => {
+        this.startGame();
+      }, 3000);
+    }
+  }
+
+  getUser(userId) {
+    return this.users.find((user) => user.id === userId);
+  }
+
+  removeUser(userId) {
+    this.users = this.users.filter((user) => user.id !== userId);
+
+    if (this.users.length < MAX_PLAYERS) {
+      this.state = GAME_STATE.WAITING;
+    }
+  }
+
+  startGame() {
+    this.state = GAME_STATE.IN_PROGRESS;
+  }
+}
+
+export default Game;
---
+++ src/constants/game.js
@@ -0,0 +1,6 @@
+export const GAME_STATE = Object.freeze({
+  WAITING: 0,
+  IN_PROGRESS: 1,
+});
+
+export const MAX_PLAYERS = 4;
