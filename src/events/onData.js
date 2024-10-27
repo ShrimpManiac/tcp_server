80cf012b0ceea1e9a3e9abddbe89708b4e784c4c
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-10-27 11:49:01 +0900

    comment: "onData 이벤트 코드설명 주석 추가"

--- src/events/onData.js
+++ src/events/onData.js
@@ -8,12 +8,18 @@ import { handleError } from '../utils/error/errorHandler.js';
 import { packetParser } from '../utils/parser/packetParser.js';
 
 export const onData = (socket) => async (data) => {
+  // 기존 버퍼에 새로 수신된 데이터를 추가
   socket.buffer = Buffer.concat([socket.buffer, data]);
 
+  // 패킷의 총 헤더 길이 (패킷 길이 정보 + 타입 정보)
   const totalHeaderLength = config.packet.totalLength + config.packet.typeLength;
 
+  // 버퍼에 최소한 전체 헤더가 있을 때만 패킷을 처리
   while (socket.buffer.length >= totalHeaderLength) {
+    // 1. 패킷 길이 정보 수신 (4바이트)
     const length = socket.buffer.readUInt32BE(0);
+
+    // 2. 패킷 타입 정보 수신 (1바이트)
     const packetType = socket.buffer.readUInt8(config.packet.totalLength);
 
     // 아직 전체 패킷이 도착하지 않음
@@ -21,6 +27,8 @@ export const onData = (socket) => async (data) => {
       break;
     }
 
+    // 3. 패킷 전체 길이 확인 후 데이터 수신
+    // 패킷 데이터를 자르고 버퍼에서 제거
     const packet = socket.buffer.subarray(totalHeaderLength);
     socket.buffer = socket.buffer.subarray(0, totalHeaderLength);
     console.log(`length: ${length}, packetType: ${packetType}`);
@@ -36,6 +44,7 @@ export const onData = (socket) => async (data) => {
 
           // 검증: 시퀀스 일치
           const user = getuserById(userId);
+          // 유저가 접속해 있는 상황에서 시퀀스 검증
           if (user && user.sequence !== sequence) {
             throw new CustomError(ErrorCodes.INVALID_SEQUENCE, `잘못된 호출값입니다.`);
           }
