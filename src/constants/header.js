87e4b41757e0dade67849fc89a2b1993f8429f73
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-10-28 20:31:58 +0900

    update: "새로운 패킷타입 추가: GAME_START, LOCATION"

--- src/constants/header.js
+++ src/constants/header.js
@@ -14,5 +14,7 @@ export const PACKET_TYPE_BYTES = 1; // 수정시 readUInt와 writeUInt 메소드
 export const PACKET_TYPE = Object.freeze({
   PING: 0,
   NORMAL: 1,
+  GAME_START: 2,
+  LOCATION: 4,
   // 추가시 해당 enum을 사용하는 switch문에 case 추가 필요
 });
